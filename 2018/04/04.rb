bc2d59acb51fe35733480850dd4852e42637d465
David Gutierrez <d.gutierrez.job@gmail.com>
2025-07-17 17:23:02 -0600

    feat: Solution of day 4 of Advent of Code

---
+++ 2018/04/04.rb
@@ -0,0 +1,62 @@
+list = []
+File.read('input.txt').each_line do |line|
+  list.push(line)
+end
+
+list = list.sort
+
+list.map! { |lines| lines.gsub(/(\[\d+-\d+-\d+\s\d+:)/, '').gsub(/\]/, '') }
+
+def to_num(n)
+  n.match(/\d+/).to_s.to_i
+end
+
+def catch_id(n)
+  n.match(/#\d+/).to_s
+end
+
+l = list.length
+i = 0
+data = []
+data_numbers = []
+id_s = Set.new
+max = []
+
+while (l - i - 1) > 0
+  id = catch_id(list[i])
+  support_array = []
+  sleep = true
+  while sleep
+    s = 1
+    if list[i + s].match?(/\d+\sf/)
+      mins_sleep = (to_num(list[i + s + 1]) - to_num(list[i + s]))
+      mins_sleep.times do |n|
+        support_array.push(to_num(list[i + s]) + n)
+      end
+      s += 1
+    else
+      sleep = false
+    end
+    i += s
+    sleep = false if list[i + 1].nil?
+
+  end
+  id_s << id
+  support_array.unshift(id)
+  data.push(support_array)
+end
+
+id_s.each do |id|
+  ordered_data = []
+  data.each do |array|
+    puts array[0]
+    ordered_data += array if array[0] == id
+  end
+  max.push(ordered_data.length)
+  new_data = ordered_data.tally
+  data_numbers.push(new_data)
+end
+
+id = max.find_index(max.max)
+
+puts data_numbers[id]
