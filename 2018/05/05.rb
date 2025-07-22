3cb47def0e6e1b5e7283532f2fa7a3f55e115c63
David Gutierrez <d.gutierrez.job@gmail.com>
2025-07-22 10:06:48 -0600

    add: minor adjustments in the if statement inside while

--- 2018/05/05.rb
+++ 2018/05/05.rb
@@ -7,7 +7,7 @@ while reaction
 
   l = polymer.length
 
-  if polymer[i].match?(/[a-z]/) && polymer[i + 1].match?(/[A-Z]/) && polymer[i].upcase == polymer[i + 1] || polymer[i].match?(/[A-Z]/) && polymer[i + 1].match?(/[a-z]/) && polymer[i].downcase == polymer[i + 1]
+  if polymer[i] != polymer[i + 1] && polymer[i].upcase == polymer[i + 1].upcase
     polymer = polymer.gsub(polymer[i] + polymer[i + 1], '')
     i = 0
   else
